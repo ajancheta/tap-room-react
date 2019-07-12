28dba3f9ad984726f545b237f18e0d115a814b52
AJ Ancheta <ashleyjancheta@gmail.com>
2019-07-12 16:41:08 -0700

    add router path to display current on tap supply or to edit beer list

--- src/components/Beer.jsx
+++ src/components/Beer.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Beer(props) {
-  return (
+  const currentlyOnTap = (
     <div>
       <style jsx>{`
       .beer-container {
         padding: 10px;
+        font-family: 'Montserrat', sans-serif;
+        color: #ECE5D6;
+        margin: 10%;
+        background-color: #213458;
+        opacity: 0.95;
       }
 
       h3 {
@@ -25,7 +30,39 @@ function Beer(props) {
       </div>
     </div>
   );
+
+  const editBeerList = (
+    <div>
+      <style jsx> {`
+      .beer-container {
+        padding: 10px;
+        font-family: 'Montserrat', sans-serif;
+        color: #ECE5D6;
+        margin: 10%;
+        background-color: #213458;
+        opacity: 0.95;
+      }
+
+      h3 {
+        margin-top: -10px;
+      }
+
+      `}</style>
+      <div className="beer-container">
+        <div className="beer-details">
+          <h2>{props.name}</h2>
+          <button onClick={() => {props.onClickDelete(props.id);}}>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+  if (props.currentRouterPath === '/admin/edit') {
+    return <div>{editBeerList}</div>;
+  } else {
+    return <div>{currentlyOnTap}</div>;
+  }
 }
+
 
 Beer.propTypes = {
   name: PropTypes.string,
