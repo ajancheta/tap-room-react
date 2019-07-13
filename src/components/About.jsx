68d1a130d03d8eb7d5aa2aadfca3f621d6edeba6
AJ Ancheta <ashleyjancheta@gmail.com>
2019-07-13 12:55:48 -0700

    edit text padding

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,24 +28,26 @@ function About() {
           margin-top: 7%;
         }
 
-        .about-container {
-          padding: 10px;
-          font-family: 'Montserrat', sans-serif;
-          color: #ECE5D6;
-          margin: 8%;
-          background-color: #c24a19;
-          opacity: 0.95;
-        }
-
       .about-container {
+        padding: 10px;
+        font-family: 'Montserrat', sans-serif;
+        color: #ECE5D6;
+        margin: 8%;
+        background-color: #c24a19;
+        opacity: 0.95;
         display: flex;
         font-size: 20px;
+        align-items: center;
       }
 
       img {
         width: 150px;
         height: 150px;
-        padding-top: 5px;
+      }
+
+      .text {
+        text-align: justify;
+        padding: 10px;
       }
       
       `}</style>
@@ -56,15 +58,13 @@ function About() {
         <div className="media-container">
           <img src={Mushroom} />
         </div>
-
         <div className="text">
           <p>The Forager's Ale House is the resource for elevating spirits through liquid love. We celebrate the unadulterated, exquisite flavors to help connect people to finding true joy and liberation.</p>
         </div>
       </div>
-
       <div className="about-container">
         <div className="text">
-          <p>Here at the The Forager's Ale House Company, we know the future of Mother Earth depends on the way that we treat her. So we brew our beer sustainably, using solar energy to power our Big Island brewery, on-site gardens to recycle our water, and even leftover grains to bake into our pizza crust for the Curious Cubenis brewpub. We think the earth is pretty great. Let’s keep it that way!
+          <p>Here at the The Forager's Ale House Company, we know the future of Mother Earth depends on the way that we treat her. So we brew our beer sustainably, using solar energy to power our Big Island brewery, on-site gardens to recycle our water, and even leftover grains to bake into our pizza crust for the Curious Cubenis brewpub. We think the earth is pretty great. Let’s keep it that way! <br /><br />
 
           We also love giving back to our community by supporting local organizations that display an authentic, innovative approach to their mission and objectives that match up with our internal values. In-kind donation requests are reviewed by a team of brewery employees.</p>
         </div>
